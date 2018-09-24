fcf787bb18303dc63f38070bedb5eb175db800d8
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-09-24 10:33:36 -0400

    created cookies program that calculates calories consumed

--- c++/cookies.cpp
+++ c++/cookies.cpp
@@ -18,19 +18,37 @@ double total_calories(int cookies) {
   return total;
 }
 
+
+#include<iostream>
+using namespace std;
+
+int how_many() {
+  int cookies;
+
+  cout << "How many cookies did you consume?" << '\n';
+  cin >> cookies;
+
+  return cookies;
+}
+
+double total_calories(int cookies) {
+  double cookies_per_serving = 30 / 10;
+  double calories_per_serving = 300;
+  double total = cookies / cookies_per_serving * calories_per_serving;
+
+  return total;
+}
+
 double total_servings(int cookies){
     double cookies_per_serving = 30 /10;
-    
-    double serving_total = cookies * cookies_per_serving;
-    
+    double serving_total = cookies / cookies_per_serving;
     return serving_total;
-    
 }
 
 double bag(int cookies) {
     double cookies_per_serving = 30 /10;
     
-    double serving_total = cookies * cookies_per_serving;
+    double serving_total = cookies / cookies_per_serving;
     
     double bag = serving_total * 10;
     
@@ -45,9 +63,7 @@ int main() {
   
   double percentage = bag(cookies);
 
-  
-  cout << "You had " << serving_total << "servings" <<'\n',
-  cout << "You consumed " << total << " calories." << '\n',
-  cout << "That is " << percentage << "%" << " of a bag" << '\n';
-
+  cout << "You had " << serving_total << " servings" <<'\n',
+  cout << "That is " << percentage << "%" << " of a bag" << '\n',
+  cout << cookies <<" cookies" <<" is " <<total << " calories." << '\n';
   }
