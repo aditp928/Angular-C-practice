e5acb9c7e985af5ec05bdfb3e05817008b04465c
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-09-24 13:41:33 -0400

    updated code to fix bugs

--- c++/cookies.cpp
+++ c++/cookies.cpp
@@ -19,26 +19,6 @@ double total_calories(int cookies) {
 }
 
 
-#include<iostream>
-using namespace std;
-
-int how_many() {
-  int cookies;
-
-  cout << "How many cookies did you consume?" << '\n';
-  cin >> cookies;
-
-  return cookies;
-}
-
-double total_calories(int cookies) {
-  double cookies_per_serving = 30 / 10;
-  double calories_per_serving = 300;
-  double total = cookies / cookies_per_serving * calories_per_serving;
-
-  return total;
-}
-
 double total_servings(int cookies){
     double cookies_per_serving = 30 /10;
     double serving_total = cookies / cookies_per_serving;
@@ -66,4 +46,5 @@ int main() {
   cout << "You had " << serving_total << " servings" <<'\n',
   cout << "That is " << percentage << "%" << " of a bag" << '\n',
   cout << cookies <<" cookies" <<" is " <<total << " calories." << '\n';
+  return 0;
   }
